e67e8fdd1bccdf1408b1a5c04683ad273c572fc4
seetharampullela <seetharampullela@gmail.com>
2026-04-23 14:00:28 +0530

    changes for auth middleware

---
+++ backend/src/middlewares/auth.js
@@ -0,0 +1,31 @@
+const jwt = require("jsonwebtoken");
+const User = require("../model/user");
+
+const userAuth = async (req, res, next) => {
+  try {
+    const { token } = req.cookies;
+    if (!token) {
+      res.status(401).send("Please Login");
+      throw new Error("Authentication failed");
+    }
+    /* 
+      1. decodedToken would look like { _id: '69b9836ef977dfdca2c30c0c', iat: 1773799587 }
+      2. _id is the secret key that we stored while login, and iat is added as default by jwt
+    */
+    const { _id } = await jwt.verify(token, process.env.JWT_SECRET_KEY);
+    // const { _id } = decodedTokenValue;
+    if (!_id) {
+      throw new Error("Invalid token!");
+    }
+    const user = await User.findById(_id);
+    if (!user) {
+      throw new Error("Invalid User");
+    }
+    req.user = user;
+    next();
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
+  }
+};
+
+module.exports = { userAuth };
